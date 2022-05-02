104735f7c3a78c156e1aee2f50495fa896042804
André Stanlley <stanlley41@gmail.com>
2022-05-02 17:35:18 -0300

    Separação das strings

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Insira suas string separadas por um espaço: ");
+string Palavras = Console.ReadLine();
+
+Console.WriteLine(Palavras);
+
+string[] Texto = Palavras.Split(' ');
+
+string PrimeiraPalavra = Texto[0];
+string SegundaPalavra = Texto[1];
+
+Console.ReadLine();
