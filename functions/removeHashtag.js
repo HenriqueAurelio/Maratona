97ea03c97ed42aa1e473619d5056dbe8d4883b94
telmiadame <telmi.a@lyncas.net>
2022-05-03 12:20:50 -0300

    teste

--- functions/removeHashtag.js
+++ functions/removeHashtag.js
@@ -2,10 +2,15 @@ function removeHashtag(word) {
     word = word.split("")
 
     while (word.indexOf("#") > -1) {
+
         let index = word.indexOf("#")
-        word.splice((index - 1), 2)
+        if (index == 0) {
+            word.splice(index, 1)
+        }else{
+            word.splice((index - 1), 2)
+        }
+        console.log(word)
     }
     return word
 }
-
 module.exports = removeHashtag
